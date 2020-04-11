0ab79d474f9416973f72fda5a499c17a7ce61cd0
Harcourt Hamsa <hamsaharcourt@gmail.com>
2020-04-11 19:11:31 +0100

    fixing issues 2

--- src/estimator.js
+++ src/estimator.js
@@ -24,7 +24,7 @@ export const NumberOfInfectedPeopleForNDays = (
   unit
 ) => {
   const days = convertToDays(digit, unit);
-  return currentlyInfected * 2 ** getFactorFromNDays(days);
+  return Math.floor(currentlyInfected * 2 ** getFactorFromNDays(days));
 };
 
 const covid19ImpactEstimator = (data) => {
