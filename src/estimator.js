b38d3fe342152a4792396cd17d876ea79f2b1d38
Harcourt Hamsa <hamsaharcourt@gmail.com>
2020-04-11 18:34:32 +0100

    pushing code

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,100 @@
-const covid19ImpactEstimator = (data) => data;
+export const convertToDays = (digit, unit) => {
+  switch (unit) {
+    case 'days':
+      return digit;
+    case 'weeks':
+      return digit * 7;
+    case 'months':
+      return digit * 31;
+    default:
+      throw new Error('invalid unit');
+  }
+};
+
+export const getFactorFromNDays = (days) => {
+  if (days < 3) {
+    return 0;
+  }
+  return Math.floor(days / 3);
+};
+
+export const NumberOfInfectedPeopleForNDays = (
+  currentlyInfected,
+  digit,
+  unit
+) => {
+  const days = convertToDays(digit, unit);
+  return currentlyInfected * 2 ** getFactorFromNDays(days);
+};
+
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {};
+  const {
+    reportedCases, timeToElapse, periodType, totalHospitalBeds
+  } = data;
+  const { avgDailyIncomePopulation, avgDailyIncomeInUSD } = data.region;
+
+  // CHALLENGE 1
+
+  // Currently infected
+  impact.currentlyInfected = reportedCases * 10;
+
+  severeImpact.currentlyInfected = reportedCases * 50;
+
+  // Infections by requested time
+  impact.infectionsByRequestedTime = NumberOfInfectedPeopleForNDays(
+    impact.currentlyInfected,
+    timeToElapse,
+    periodType
+  );
+
+  severeImpact.infectionsByRequestedTime = NumberOfInfectedPeopleForNDays(
+    severeImpact.currentlyInfected,
+    timeToElapse,
+    periodType
+  );
+
+  // CHALLENGE 2
+
+  // Severe cases by Requested time
+  impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
+
+  severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
+
+  // Hospital Beds By Requested Time
+  impact.hospitalBedsByRequestedTime = 0.35
+  * totalHospitalBeds - impact.severeCasesByRequestedTime;
+
+  severeImpact.hospitalBedsByRequestedTime = 0.35
+  * totalHospitalBeds - severeImpact.severeCasesByRequestedTime;
+
+  // CHALLENGE 3
+
+  // ICU care
+  impact.casesForICUByRequiredTime = 0.05 * impact.infectionsByRequestedTime;
+
+  severeImpact.casesForICUByRequiredTime = 0.05 * severeImpact.infectionsByRequestedTime;
+
+  // Ventilators
+  impact.casesForVentilatorsByRequiredTime = 0.02 * impact.infectionsByRequestedTime;
+
+  severeImpact.casesForVentilatorsByRequiredTime = 0.02 * severeImpact.infectionsByRequestedTime;
+
+  // Dollars in Flight
+  impact.dollarsInFlight = (impact.infectionsByRequestedTime * avgDailyIncomePopulation)
+    * avgDailyIncomeInUSD
+    * convertToDays(timeToElapse, periodType);
+
+  severeImpact.dollarsInFlight = (severeImpact.infectionsByRequestedTime * avgDailyIncomePopulation)
+    * avgDailyIncomeInUSD
+    * convertToDays(timeToElapse, periodType);
+
+  return {
+    data, // Input data
+    impact, // Best case scenario
+    severeImpact // Worst case scenario
+  };
+};
 
 export default covid19ImpactEstimator;
---
+++ src/estimator.test.js
@@ -0,0 +1,51 @@
+import covid19ImpactEstimator, {
+  getFactorFromNDays,
+  NumberOfInfectedPeopleForNDays,
+  convertToDays
+} from './estimator';
+
+describe('covid19ImpactEstimator', () => {
+  it('should return an object with data, impact and severeImpact as its properties', () => {
+    const data = {
+      region: {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+      },
+      periodType: 'days',
+      timeToElapse: 58,
+      reportedCases: 674,
+      population: 66622705,
+      totalHospitalBeds: 1380614
+    };
+
+    const result = covid19ImpactEstimator(data);
+    expect(result).toHaveProperty('data');
+    expect(result).toHaveProperty('impact');
+    expect(result).toHaveProperty('severeImpact');
+  });
+});
+
+describe('getFactorFromNDays', () => {
+  it('should return the factor for a given number of days ', () => {
+    expect(getFactorFromNDays(1)).toEqual(0);
+    expect(getFactorFromNDays(30)).toEqual(10);
+    expect(getFactorFromNDays(53)).toEqual(17);
+  });
+});
+
+
+describe('infectedPeopleForNDays', () => {
+  it('should double the number of infected people every 3 days', () => {
+    expect(NumberOfInfectedPeopleForNDays(1, 30, 'days')).toEqual(1024);
+  });
+});
+
+describe('convertToDays', () => {
+  it('should convert the  input to days depending on the unit given', () => {
+    expect(convertToDays(3, 'weeks')).toEqual(21);
+    expect(convertToDays(2, 'months')).toEqual(62);
+    expect(convertToDays(2, 'days')).toEqual(2);
+  });
+});
